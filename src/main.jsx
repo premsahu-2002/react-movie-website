70233877e0fa348286d0c38eddd6eb0c5926a4c5
Prem sahu <prem41121@gamil.com>
2025-07-02 14:12:13 +0530

    fixed small error

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,31 @@
-import { createRoot } from 'react-dom/client'
-import Root from './Root.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './pages/ErrorPage.jsx'
-import CtxProvider from './provider/global-state-provider.jsx'
-import MovieSeprate from './pages/MovieSeprate.jsx'
-import Index from './index.jsx'
+import { createRoot } from "react-dom/client";
+import Root from "./Root.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import CtxProvider from "./provider/global-state-provider.jsx";
+import MovieSeprate from "./pages/MovieSeprate.jsx";
+import Index from "./Index.jsx";
 
-
-const router = createBrowserRouter([{
-    path: '/',
+const router = createBrowserRouter([
+  {
+    path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [{
+    children: [
+      {
         index: true,
         element: <Index />,
-    },{
-        path: '/movie/:movieId',
+      },
+      {
+        path: "/movie/:movieId",
         element: <MovieSeprate />,
-    }
-]
-}
-])
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
-
-    <CtxProvider>
+createRoot(document.getElementById("root")).render(
+  <CtxProvider>
     <RouterProvider router={router} />
-    </CtxProvider>
-   
-)
+  </CtxProvider>
+);
